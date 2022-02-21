eca9fc5a5b93aa9f6697018fe87481eaae23cc00
@naive_coder <prashant123.iimt@gmail.com>
2022-02-21 18:39:00 +0530

    Another Todo List

---
+++ main.js
@@ -0,0 +1,20 @@
+let add = document.getElementById("add");
+let todocontainer = document.getElementById("todocontainer");
+let input = document.getElementById("input");
+
+add.addEventListener('click', function () {
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = input.value;
+    todocontainer.appendChild(paragraph);
+    input.value = "";
+    paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = "line-through";
+        paragraph.addEventListener('click', function () {
+            paragraph.style.textDecoration = "none";
+        });
+    });
+    paragraph.addEventListener('dblclick', function () {
+        todocontainer.removeChild(paragraph);
+    });
+});
